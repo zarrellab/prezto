698c621a405fdd26ef614538962a173a9b2a47ca
Brandon Zarrella <zarrellab@gmail.com>
2020-11-15 15:01:37 -0700

    add openjdk to jenv

--- scripts/init-utils.zsh
+++ scripts/init-utils.zsh
@@ -5,14 +5,20 @@ echo 'installing related utils...'
 # vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Base16 color tools
 base16-manager install chriskempson/base16-shell
 base16-manager install chriskempson/base16-vim
 base16-manager set spacemacs
+
 # Node in node version manager
 nvm
 nvm install lts
 npm i -g npm
+
 # Neovim utils
 pip3 install pynvim
 npm i -g neovim
+
+# Set java in jenv
+jenv add /usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home
